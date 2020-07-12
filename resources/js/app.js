ed659d61adc8d1224bf0d180f863db5a34505583
indrapwa_laptop <gensbookcities@gmail.com>
2020-07-12 22:35:25 +0700

    testing

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,37 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
+Vue.component('chat-form', require('./components/ChatForm.vue').default);
+
 const app = new Vue({
     el: '#app',
+    data: {
+        messages: []
+    },
+    created() {
+        this.fetchMessages();
+    },
+    methods: {
+        fetchMessages() {
+            axios.get('/messages').then(response => {
+                this.messages = response.data;
+            });
+            Echo.private('chat')
+                .listen('MessageSent', (e) => {
+                    console.log(e);
+                    this.messages.push({
+                        message: e.message.message,
+                        user: e.user
+                    });
+                });
+        },
+        addMessage(message) {
+            this.messages.push(message);
+
+            axios.post('/messages', message).then(response => {
+                console.log(response.data);
+            });
+        }
+    }
 });
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('chatting', 'MessageController@index');
+Route::get('messages', 'MessageController@fetchMessages');
+Route::post('messages', 'MessageController@sendMessage');
